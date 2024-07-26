b8e7bb039e35e34ed47aeac88aa0288adca05977
Miguel Buriticá <miguelaburiticav@gmail.com>
2024-07-25 22:37:21 -0500

    refactor: Update ClickeableCard component to improve code readability and maintainability

--- src/components/ClickeableCard.vue
+++ src/components/ClickeableCard.vue
@@ -1,22 +1,33 @@
+<template>
+  <article
+    @click="callback"
+    :class="
+      `flex justify-between items-center w-full h-28 relative
+      rounded-2xl p-8 shadow-md border-rose-400 border-2 transition-all duration-300
+      hover:shadow-xl hover:scale-105
+      ${callback && 'cursor-pointer'}
+      `
+    ">
+    <div class="flex items-center justify-start gap-6">
+      <div class=" size-12 bg-blue-500 rounded-2xl"></div>
+      <label class=" text-3xl">{{ title }}</label>
+    </div>
+
+    <div v-if="url" @click="onClick" class="w-8 h-8 bg-green-500 rounded-full cursor-pointer"></div>
+  </article>
+</template>
+
 <script setup lang="ts">
 import { type ClickeableElement } from "../types";
 
 const props = defineProps<ClickeableElement>();
+
+const onClick = () => {
+  if (props.url) {
+    console.log('Redirecting to', props.url)
+    window.open(props.url, '_blank')
+  }
+  else if (props.callback) props.callback()
+}
+
 </script>
-
-<template>
-  <article
-    @click="props.callback"
-    class="
-      flex justify-between items-center w-full h-16
-      rounded-md p-8
-      shadow-md border-rose-400 border-2
-    ">
-    <div class="flex items-center justify-start">
-      <div class="w-8 h-8 bg-blue-500 rounded-full"></div>
-      <div class="ml-4">{{ props.title }}</div>
-    </div>
-
-    <div v-if="props.url" @click="() => console.log('AHHHHH')" class="w-8 h-8 bg-green-500 rounded-full"></div> 
-  </article>
-</template>
